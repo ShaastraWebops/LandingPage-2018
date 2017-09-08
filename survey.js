ec4325c833dabdba48815cfb706da90c25990d88
rohithram <ram234historyalex@gmail.com>
2017-09-08 20:47:11 +0530

    Added workshops form

--- survey.js
+++ survey.js
@@ -4,7 +4,7 @@ var workshops = ["Python", "Arduino", "Matlab", "Web Development", "Image Proces
     "Fusion",  "R", "Eagle, PCB Design", "Cosmic Presentation", "Photoshop & Lightroom", "After Effects",
     "E-Commerce", "Ethical Hacking", "Drone Demonstration", "Advanced Programming Workshop"];
 
-app.controller('main', function($http){
+app.controller('main', function($http, $scope){
 
 	this.workshops = workshops;
 	
@@ -12,4 +12,23 @@ app.controller('main', function($http){
 		this.citys = res.data;
 		console.log(this.citys);
 	})
+
+	this.submitForm = function(){
+
+		console.log("Entered submitform");
+		var workshoplist = [];
+		if($scope.user.workshop1!=null)
+			workshoplist.push({'name': $scope.user.workshop1});
+		if($scope.user.workshop2!=null)
+			workshoplist.push({'name': $scope.user.workshop2});
+		if($scope.user.workshop3!=null)
+			workshoplist.push({'name': $scope.user.workshop3});
+
+		console.log(workshoplist);
+
+		$http.put('http://shaastra.org:8080/api/citys/'+$scope.user.city._id,{
+			workshops: workshoplist
+		}).then(res => {
+		})
+	}
 })

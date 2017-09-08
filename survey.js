52371d6d312098ee5b21ef4cf62c7453b35d31cc
Gokulan <gokulan97@gmail.com>
2017-09-09 01:39:57 +0530

    Survey Completed. Few validations need to be done

--- survey.js
+++ survey.js
@@ -10,50 +10,67 @@ app.controller('main', function($http, $scope){
 	
 	$http.get('http://shaastra.org:8080/api/citys').then(res => {
 		this.citys = res.data;
-		console.log(this.citys);
-	})
+	});
+
+	this.addCity = function(){
+		ws = []
+		for(w in workshops)
+		{
+			ws.push({
+				name: w,
+				count:0
+			});
+		}
+		$http.post('http://shaastra.org:8080/api/citys', {
+			name: this.city1,
+			workshops: ws
+		}).then(res => {console.log(res.status);});
+	}
 
 	this.submitForm = function(){
+		this.subCity = {
+			name: "",
+			workshops: []
+		};
 		
-		$http.get('http://shaastra.org:8080/api/citys').then(res => {
-			this.citys1 = res.data;
-			console.log(this.citys1);
-			for(var j=0; j<this.citys1.length; j++){
-				if(this.citys1[j].name === $scope.user.city.name)
-					$scope.user.city.workshops = this.citys1[j].workshops;
+		$http.get('http://shaastra.org:8080/api/citys/'+this.city).then(res => {
+			this.subCity = res.data;
+
+			name = this.subCity.name;
+			workshops = []
+			for(var i=0; i<this.subCity.workshops.length;i++)
+			{
+				if(this.subCity.workshops[i].name === this.workshop1)
+				{
+					workshops.push({
+						name: this.subCity.workshops[i].name,
+						count: this.subCity.workshops[i].count+1
+					});
+				}
+				else if(this.subCity.workshops[i].name === this.workshop2)
+				{
+					workshops.push({
+						name: this.subCity.workshops[i].name,
+						count: this.subCity.workshops[i].count+1
+					});
+				}
+				else if(this.subCity.workshops[i].name === this.workshop3)
+				{
+					workshops.push({
+						name: this.subCity.workshops[i].name,
+						count: this.subCity.workshops[i].count+1
+					});
+				}
+				else
+					workshops.push(this.subCity.workshops[i]);
 			}
-		var count1=0,count2=0,count3=0;
-
-		if($scope.user.city.workshops.length!=0){
-			for(var i=0; i<$scope.user.city.workshops.length;i++){
-				if($scope.user.city.workshops[i].name === $scope.user.workshop1){
-					count1 = $scope.user.city.workshops[i].count;
-				}
-				if($scope.user.city.workshops[i].name === $scope.user.workshop2){
-					count2 = $scope.user.city.workshops[i].count;
-				}
-				if($scope.user.city.workshops[i].name === $scope.user.workshop3){
-					count3 = $scope.user.city.workshops[i].count;
-				}
-			}
-		}
-		console.log("Entered submitform");
-		var workshoplist = [];
-		if($scope.user.workshop1!=null)
-			workshoplist.push({'name': $scope.user.workshop1, 'count': count1});
-		if($scope.user.workshop2!=null)
-			workshoplist.push({'name': $scope.user.workshop2, 'count': count2});
-		if($scope.user.workshop3!=null)
-			workshoplist.push({'name': $scope.user.workshop3, 'count': count3});
-
-		console.log(workshoplist);
-
-		$http.put('http://shaastra.org:8080/api/citys/'+$scope.user.city._id,{
-			workshops: workshoplist
-		}).then(res => {
-		})
-		})
-		
-		
+			
+			$http.put('http://shaastra.org:8080/api/citys/'+this.city, {
+				name: this.name,
+				workshops: workshops
+			}).then(res => {
+				console.log(res.status);
+			})
+		});		
 	}
 })
